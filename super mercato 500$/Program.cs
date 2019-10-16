f975388e187d2a66ae9a12763bbd25acec194c20
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:56:09 +0200

    Add project files.

---
+++ super mercato 500$/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace super_mercato_500_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci il prezzo totale: ");
+            double totale = double.Parse(Console.ReadLine());
+            double s = totale * 20 / 100;
+            double s2 = totale * 10 / 100;
+            if (totale > 500)
+            {
+                double totalescontato = totale - s;
+                Console.WriteLine($"il prezzo scontato è:{totalescontato} ");
+            }
+            else
+            {
+                double totalescontato2 = totale - s2;
+                Console.WriteLine($"l' importo è: {totalescontato2}");
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
